d6bf10159a813857ac509ac71a3c06240287b2a2
mahirnpatel <mahirpatel3107@gmail.com>
2022-08-18 16:05:17 +0530

    exporting the module

---
+++ Node_js/ExportModule.mjs
@@ -0,0 +1,12 @@
+export function simple() {
+
+
+    console.log("Exporting module");
+}
+export  function simple1() {
+
+
+    console.log("Exporting module1");
+}
+
+// module.exports = ExportingFunction;
