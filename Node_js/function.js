e0b20dfd50eb7a4378c0889e84e04427decfe7ff
mahirnpatel <mahirpatel3107@gmail.com>
2022-08-18 16:03:38 +0530

    functions in node js

---
+++ Node_js/function.js
@@ -0,0 +1,23 @@
+// function add (x , y){
+
+//     return x+y;
+// }
+
+        //OR
+// let add  = (x,y) => {
+
+//     return x+y;
+// }
+
+//THERE IS THE ANOTHER WAY TO EXPORT THE MODULE
+exports.add = (x , y) => {
+    return x+y;
+}
+
+
+
+//THIS IS THE WAY TO CALL THE FUNCTION AND PRINT THE RESULT;
+
+// let result = add(10 ,10);
+// console.log("The result is : " , result);
+
